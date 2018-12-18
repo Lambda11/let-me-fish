923f77b4d351f54bd1a92504971b5a9502f086df
Owyn <Owyn@users.noreply.github.com>
2018-12-19 00:04:51 +0300

    Becky did let me fish

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# let-me-fish
-Becky pls! lemme fish!
+<img src=http://u.cubeupload.com/Owyn/lemmefish.jpg>
+
+# Let-me-Fish
+TERA-proxy module that mass-auto-fishes fishes, auto-crafts bait when out of bait, and auto-dismantles fish when inventory gets full.
+
+Emulates human-like behavior mostly, abides human-like delays for actions based on fishing diffculty, retries if something goes wrong, works well even if the game is very limited in CPU resources.
+
+Supports `auto-nego`tiate (my fork https://github.com/Owyn/auto-nego ), so you would be able to both auto-fish and auto-negotiate
+
+## Usage
+Use command `fish` then follow instruction as simple as just throwing your fish-rod and it will auto-continue everything for you
+
+`fish save` - saves your used recipe and list of fishes to auto-dismantle into per-character config file which will be used on next login
+`fish reset` - resets your loaded config
+`fish list` - lists settings used
+`fish reload` - reloads config file (if you edited it manually and want to reload it right now)
+
+- you can set a different recipe or add fishes to auto-dismantle-list after using `fish` command but before throwing your rod any time
+
+
+## Installation
+put `defs` insides to `node_modules\tera-data\protocol\` folder
+put `opcodes` insides to `node_modules\tera-data\map\` (in Caalis proxy)
+
+(You might need to do additional manual updates if you'r using Pinkies proxy, but you must be used to that already with it)
+
+## Notes
+
+- Stops when you get 10k fishlets (if you want to auto-discard fishlets - reconsider your fishing life in tera, fishlets are the main purpose for fishing after you upgrade your rod... ofc you could use side modules for that like https://github.com/teralove/no-strongboxes and adding fishlet id there)
+
+- Opcodes are gotten via third-party sources mostly (submitted by users), if there are no opcodes for your region - too bad, you'd have to get em yourself, can't help you here, use https://github.com/Owyn/alex-packet-id-finder or https://github.com/Owyn/debug-logger/
+
+- Yes, you can make it catch fish much faster like other cheating fishers do, even like in 2 sec lol, but server might decide to start checking how long it takes for ppl to actually catch  fish and ban all unreasonably fast fishers, so only way to change this timing is to set `disableAutoUpdate` to `true` in `module.json` and edit top lines in the `index.js` file manually at your very own risk
+
+- Make sure you have some free slots in the inventory before fishing and some bait\fishlets to craft the bait before you start the `fish`
---
+++ index.js
@@ -0,0 +1,638 @@
+const	ACTION_DELAY_THROW_ROD	= [6123, 6798],		// [Min, Max] in ms, 1000 ms = 1 sec
+		ACTION_DELAY_FISH_START	= [845,  2656],		// [Min, Max] - the pressing of F button to reel and start the minigame
+		ACTION_DELAY_FISH_CATCH	= [6564, 16453],	// [Min, Max] - time to win the fishing minigame and get a fish as prize
+		DELAY_BASED_ON_FISH_TIER= true; // tier 4 would get caught 4 sec longer, BAF (tier 11) would get caught 11 sec longer etc
+
+const   path = require('path'),
+		fs = require('fs');
+				
+const BAIT_RECIPES = [
+	{name: "Bait II",	itemId: 206001, recipeId: 204100},
+	{name: "Bait III",	itemId: 206002, recipeId: 204101},
+	{name: "Bait IV",	itemId: 206003, recipeId: 204102},
+	{name: "Bait V",	itemId: 206004, recipeId: 204103}
+];
+		
+module.exports = function LetMeFish(mod) {
+	const command = mod.command;
+	
+	let enabled = false,
+		scanning = false,
+		too_much_fishes = false,
+		triedDismantling = false,
+		myGameId = 0n,
+		statFished = 0,
+		statFishedTiers = {},
+		hooks = [],
+		toDismantle = {},
+		thefishes = [],
+		curTier = 0,
+		timer = null,
+		rodId = 0,
+		baitId = 0,
+		craftId = 0,
+		leftArea = 0,
+		playerLoc = null,
+		vContractId = null,
+		invenItems = [],
+		statStarted = null,
+		gSettings = {},
+		settingsFileName,
+		hasNego = mod.manager.isLoaded('auto-nego'),
+		pendingDeals = [],
+		negoWaiting = false;
+	
+	function saveSettings(obj)
+	{
+		if (Object.keys(obj).length)
+		{
+			try
+			{
+				fs.writeFileSync(path.join(__dirname, settingsFileName), JSON.stringify(obj, null, "\t"));
+			}
+			catch (err)
+			{
+				command.message("Error saving settings " + err);
+				return false;
+			}
+		}
+	}
+
+	function loadSettings()
+	{
+		try
+		{
+			return JSON.parse(fs.readFileSync(path.join(__dirname, settingsFileName), "utf8"));
+		}
+		catch (err)
+		{
+			//console.log("Error loading settings " + err);
+			return {};
+		}
+	}
+	
+	if(!fs.existsSync(path.join(__dirname, './saves')))
+	{
+		fs.mkdirSync(path.join(__dirname, './saves'));
+	}
+
+	command.add('fish', {
+        $default() {
+            enabled = !enabled;
+			command.message(`Let me Fish is now: ${enabled ? "enabled" : "disabled"}.`);
+			if(enabled)
+			{
+				start();
+				scanning = true;
+				if(!craftId)
+				{
+					command.message("1) Click craft on a bait recipe you want to auto-craft");
+				}
+				if(!Object.keys(toDismantle).length)
+				{
+					command.message("2) Put all fish-types you want to auto-dismantle into dismantle window");
+				}
+				command.message("3) Throw your rod - and it will auto-start");
+			}
+			else
+			{
+				Stop();
+			}
+        },
+		reset() {
+			toDismantle = {};
+			craftId = 0;
+			baitId = 0;
+			command.message("Craft recipe reseted");
+			command.message("Bait type for reuse reseted");
+			command.message("List of fishes to auto-dismantle reseted");
+		},
+		list() {
+			command.message("Recipe for auto-craft: " + (craftId ? craftId : "none"));
+			command.message("Bait for reusing after craft: " + (baitId ? baitId : "none"));
+			command.message("Fish-List to auto-dismantle (" + (Object.keys(toDismantle).length) + "):");
+			if(Object.keys(toDismantle).length)
+			{
+				for(let i in toDismantle)
+				{
+					command.message(i);
+				}
+			}
+			else
+			{
+				command.message("none");
+			}
+		},
+		save() {
+			command.message("Settings saved and would be carried over to next session on this character");
+			gSettings.toDismantle = toDismantle;
+			gSettings.craftId = craftId;
+			gSettings.baitId = baitId;
+			saveSettings(gSettings);
+		},
+		load() {
+			command.message("reLoaded settings file");
+			gSettings = loadSettings();
+		}
+	});
+	
+	function addZero(i) 
+	{
+		if (i < 10) {
+			i = "0" + i;
+		}
+		return i;
+	}
+	
+	function rng([min, max])
+	{
+		return min + Math.floor(Math.random() * (max - min + 1));
+	}
+	
+	function Stop()
+	{
+		enabled = false
+		vContractId = null;
+		unload();
+		clearTimeout(timer);
+		if(!scanning)
+		{
+			let d = new Date();
+			let t = d.getTime();
+			let timeElapsedMSec = t-statStarted;
+			d = new Date(1970, 0, 1); // Epoch
+			d.setMilliseconds(timeElapsedMSec);
+			let h = addZero(d.getHours());
+			let m = addZero(d.getMinutes());
+			let s = addZero(d.getSeconds());
+			command.message('Fished out: ' + statFished + ' fishes. Time elapsed: ' + (h + ":" + m + ":" + s) + ". Per fish: " + Math.round((timeElapsedMSec / statFished) / 1000) + " sec");
+			command.message('Fishes: ');
+			for(let i in statFishedTiers)
+			{
+				command.message('Tier ' + i + ': ' + statFishedTiers[i]);
+			}
+			statFished = 0;
+			statFishedTiers = {};
+		}
+		else
+		{
+			command.message('You decided not to fish?');
+		}
+	}
+	
+	function reel_the_fish()
+	{
+		mod.toServer("C_START_FISHING_MINIGAME", 1, {});
+	}
+	
+	function catch_the_fish()
+	{
+		statFished++;
+		mod.toServer("C_END_FISHING_MINIGAME", 1, {success:true});
+		timer = setTimeout(throw_the_rod, rng(ACTION_DELAY_THROW_ROD));
+	}
+	
+	function throw_the_rod()
+	{
+		if(pendingDeals.length)
+		{
+			command.message("Lets address suggested deals and give it some time...");
+			//console.log("nego start wait");
+			
+			for(let i = 0; i < pendingDeals.length; i++)
+			{
+				mod.toClient('S_TRADE_BROKER_DEAL_SUGGESTED', 1, pendingDeals[i]);
+				pendingDeals.splice(i--, 1);
+			}
+			negoWaiting = true;
+			timer = setTimeout(throw_the_rod, (rng(ACTION_DELAY_THROW_ROD)*6));
+		}
+		else if(rodId)
+		{
+			negoWaiting = false;
+			mod.toServer('C_USE_ITEM', 3, {
+				gameId: myGameId,
+				id: rodId,
+				dbid: 0n, // dbid is sent only when used from inventory, but not from quickslot
+				target: 0n,
+				amount: 1,
+				dest: 0,
+				loc: playerLoc.loc,
+				w: playerLoc.w,
+				unk1: 0,
+				unk2: 0,
+				unk3: 0,
+				unk4: true
+			});
+		}
+		else
+		{
+			command.message("You didn't use your rod item when you was told to, did you? Now let-me-fish can't rethrow it for you...");
+			Stop();
+		}
+	}
+	
+	function use_bait_item()
+	{
+		if(baitId)
+		{
+			mod.toServer('C_USE_ITEM', 3, {
+				gameId: myGameId,
+				id: baitId,
+				dbid: 0n, // dbid is sent only when used from inventory, but not from quickslot
+				target: 0n,
+				amount: 1,
+				dest: 0,
+				loc: playerLoc.loc,
+				w: playerLoc.w,
+				unk1: 0,
+				unk2: 0,
+				unk3: 0,
+				unk4: true
+			});
+			timer = setTimeout(throw_the_rod, rng(ACTION_DELAY_THROW_ROD));
+		}
+		else
+		{
+			command.message("How can you fish without a bait?... hmmm...");
+			Stop();
+		}
+	}
+	
+	function cleanup_by_dismantle()
+	{
+		if(enabled)
+		{
+			if(Object.keys(toDismantle).length)
+			{
+				thefishes.length = 0;
+				for(let i in toDismantle)
+				{
+					 thefishes = thefishes.concat(invenItems.filter((item) => item.id === parseInt(i,10)));
+				}
+				if(thefishes.length > 20)
+				{
+					too_much_fishes = true;
+					while(thefishes.length > 20)
+					{
+						thefishes.pop();
+					}
+				}
+				else
+				{
+					too_much_fishes = false;
+				}
+				if(thefishes.length)
+				{
+					command.message("Gonna dismantle this much fishes: " + thefishes.length);
+					if(!vContractId)
+					{
+						mod.toServer('C_REQUEST_CONTRACT', 1, {type: 89});
+					}
+					timer = setTimeout(dismantle_put_in_one_fish, rng(ACTION_DELAY_FISH_START));
+				}
+				else
+				{
+					command.message("No fishes found in your inventory, can't free up space, stopping");
+					console.log("No fishes found in your inventory, can't free up space, stopping");
+					Stop();
+				}
+			}
+			else
+			{
+				command.message("You didn't provide a list of fishes to auto-dismantle, did you? Now let-me-fish can't free up inventory space for you...");
+				Stop();
+			}
+		}
+	}
+	
+	function dismantle_put_in_one_fish()
+	{
+		if(vContractId)
+		{
+			const thefish = thefishes.pop();
+			
+			mod.toServer('C_RQ_ADD_ITEM_TO_DECOMPOSITION_CONTRACT', 1, {
+				contractId: vContractId,
+				dbid: thefish.dbid,
+				id: thefish.id,
+				count: 1
+			});
+
+			if(thefishes.length)
+			{
+				timer = setTimeout(dismantle_put_in_one_fish, (rng(ACTION_DELAY_FISH_START)/4));
+			}
+			else
+			{
+				timer = setTimeout(dismantle_start0, (rng(ACTION_DELAY_FISH_START)/2));
+			}
+		}
+		else
+		{
+			command.message("Hmmm... we didn't get a contract for dismantlying for some reason");
+			Stop();
+		}
+	}
+	
+	function dismantle_start0()
+	{
+		mod.toServer('C_RQ_START_SOCIAL_ON_PROGRESS_DECOMPOSITION', 1, { contract: vContractId });
+		timer = setTimeout(dismantle_start, 1925);
+	}
+	
+	function dismantle_start()
+	{
+		mod.toServer('C_RQ_COMMIT_DECOMPOSITION_CONTRACT', 1, {contract: vContractId});
+		if(too_much_fishes)
+		{
+			cleanup_by_dismantle();
+		}
+		else
+		{
+			setTimeout(dismantle_start2, rng(ACTION_DELAY_FISH_START)); // lets not let user cancel that
+		}
+	}
+	
+	function dismantle_start2()
+	{
+		mod.toServer('C_CANCEL_CONTRACT', 1, {
+			type: 89,
+			id: vContractId
+		});
+		vContractId = null;
+		timer = setTimeout(throw_the_rod, rng(ACTION_DELAY_THROW_ROD)); // lets resume fishing
+	}
+	
+	function craft_bait_start()
+	{
+		if(craftId)
+		{
+			let filets = invenItems.find((item) => item.id === 204052);
+			if(filets && filets.amount >= 30)
+			{
+				triedDismantling = false;
+				mod.toServer('C_START_PRODUCE', 1, {recipe:craftId, unk: 0});
+			}
+			else if(!triedDismantling)
+			{
+				triedDismantling = true;
+				timer = setTimeout(cleanup_by_dismantle, rng(ACTION_DELAY_THROW_ROD));
+				command.message("You don't have enough fish parts to craft a bait... dismantling fishes to get some");
+			}
+			else
+			{
+				command.message("You don't have enough fish parts to craft a bait and no fish to dismantle for fish parts... stopping");
+				console.log("You don't have enough fish parts to craft a bait  and no fish to dismantle for fish parts... stopping");
+				Stop();
+			}
+		}
+		else
+		{
+			command.message("You didn't provide a sample craft recipe, did you? Now let-me-fish can't craft more bait for you...");
+			Stop();
+		}
+	}
+
+	mod.hook('C_PLAYER_LOCATION', 5, event => {
+		playerLoc = event;
+	});
+
+	mod.hook('S_LOGIN', 12, event => {
+		myGameId = event.gameId;
+		invenItems = [];
+		rodId = null;
+		vContractId = null;
+		settingsFileName = `./saves/${event.name}-${event.serverId}.json`;
+		gSettings = loadSettings();
+		if(!Object.keys(gSettings).length)
+		{
+			baitId = 0;
+			craftId = 0;
+			toDismantle = {}
+		}
+		else
+		{
+			toDismantle = gSettings.toDismantle;
+			craftId = gSettings.craftId;
+			baitId = gSettings.baitId;
+			/*console.log("LOADED SETTINGS: ");
+			console.log(toDismantle);
+			console.log(craftId);
+			console.log(baitId);*/
+		}
+	});
+		
+	function start()
+	{
+		if(hooks.length) return;
+		
+		Hook('S_START_FISHING_MINIGAME', 1, event => {
+			if (!enabled || scanning) return;
+			
+			//let eventgameId = BigInt(data.readUInt32LE(8)) | BigInt(data.readUInt32LE(12)) << 32n;
+			if(myGameId === event.gameId)
+			{
+				let fishTier = event.level; //data.readUInt8(16);
+				if(DELAY_BASED_ON_FISH_TIER)
+				{
+					curTier = fishTier;
+				}
+				statFishedTiers[fishTier] = statFishedTiers[fishTier] ? statFishedTiers[fishTier]+1 : 1;
+				//console.log("size of statFishedTiers now: " + (Object.keys(statFishedTiers).length));
+				//console.log(statFishedTiers);
+				command.message("Started fishing minigame, Tier: " + fishTier);
+				timer = setTimeout(catch_the_fish, (rng(ACTION_DELAY_FISH_CATCH)+(curTier*1000)));
+				return false; // lets hide that minigame
+			}
+		});
+		
+		Hook('S_FISHING_BITE', 1, event => {
+			if (!enabled) return;
+			
+			//let eventgameId = BigInt(data.readUInt32LE(8)) | BigInt(data.readUInt32LE(12)) << 32n;
+			if(myGameId === event.gameId)
+			{
+				timer = setTimeout(reel_the_fish, rng(ACTION_DELAY_FISH_START));
+				leftArea = 0;
+				if(scanning)
+				{
+					scanning = false;
+					rodId = event.rodId;
+					let d = new Date();
+					statStarted = d.getTime();
+					command.message("Rod set to: " + rodId);
+					if(!craftId)
+					{
+						command.message("You didn't provide a bait recipe for auto-craft, let-me-fish will stop once it runs out of bait...");
+					}
+					if(!Object.keys(toDismantle).length)
+					{
+						command.message("You didn't provide a list of fishes to auto-dismantle, let-me-fish will stop once inventory runs out of space...");
+					}
+					command.message("Auto-fishing is started now");
+				}
+				command.message("Fish got your bait ");
+				return false; // lets hide and enjoy peace of mind with no temptation to smash "F" button
+			}
+		});
+		
+		Hook('S_INVEN', 16, event => {
+			if(!enabled) return;
+			
+			invenItems = event.first ? event.items : invenItems.concat(event.items);
+		});
+		
+		Hook('C_RQ_ADD_ITEM_TO_DECOMPOSITION_CONTRACT', 1, event =>{
+			if(!scanning) return;
+			
+			toDismantle[event.id] = true;
+			command.message("Now fish with this id would get auto-dismantled: " + event.id);
+			//console.log("size of toDismantle now: " + (Object.keys(toDismantle).length));
+			//console.log(toDismantle);
+		});
+		
+		Hook('S_REQUEST_CONTRACT', 1, event =>{
+			if(!enabled || scanning || event.type != 89 || event.senderId !== myGameId) return;
+			
+			vContractId = event.id;
+			command.message("Got the contract id for dismantling: " + event.id);
+		});
+		
+		Hook('S_CANCEL_CONTRACT', 1, event =>{
+			if(!enabled || scanning || event.type != 89 || event.id != vContractId || event.senderId !== myGameId) return;
+			
+			vContractId = null;
+			command.message("Contract for dismantling cancelled (not by let-me-fish), retrying dismantling sequence...");
+			clearTimeout(timer);
+			timer = setTimeout(cleanup_by_dismantle, rng(ACTION_DELAY_THROW_ROD));
+		});
+
+		Hook('C_START_PRODUCE', 1, event =>{
+			if(!scanning) return;
+			
+			craftId = event.recipe;
+			let found = BAIT_RECIPES.find(obj => obj.recipeId === event.recipe);
+			if(found)
+			{
+				baitId = found.itemId;
+				command.message("Now this recipe would get crafted when out of bait: " + event.recipe + ", and this bait would be activated after: " + baitId);
+			}
+			else
+			{
+				command.message("What the hell did you just craft instead of a bait?! Go craft some bait!");
+			}
+		});
+		
+		Hook('S_END_PRODUCE', 1, event =>{
+			if(!enabled || scanning) return;
+			
+			if(event.success)
+			{
+				craft_bait_start(); // no need to wait, client doesn't (when you click "craft all")
+			}
+		});
+		
+		Hook('S_TRADE_BROKER_DEAL_SUGGESTED', 1, event => {
+			if(hasNego && !negoWaiting && event.offeredPrice === event.sellerPrice) // lets take a break and trade shall we?
+			{
+				for(let i = 0; i < pendingDeals.length; i++)
+				{
+					let deal = pendingDeals[i];
+					if(deal.playerId == event.playerId && deal.listing == event.listing) pendingDeals.splice(i--, 1);
+				}
+				pendingDeals.push(event);
+				//console.log("nego deal suggested");
+				command.message("Nego deal was suggested, gonna address it after current fish...")
+				return false;
+			}
+		});
+		
+		Hook('S_SYSTEM_MESSAGE', 1, event => {
+			if(!enabled || scanning) return;
+			const msg = mod.parseSystemMessage(event.message);
+			//command.message(msg.id);
+			
+			if(msg.id === 'SMT_CANNOT_FISHING_NON_BAIT') // out of bait
+			{
+				command.message("Out of bait, lets craft some!");
+				clearTimeout(timer);
+				timer = setTimeout(craft_bait_start, rng(ACTION_DELAY_FISH_START));
+			}
+			else if(msg.id === 'SMT_ITEM_CANT_POSSESS_MORE') // craft limit
+			{
+				if(!vContractId)
+				{
+					command.message("Crafted to the fullest, lets fish again!");
+					clearTimeout(timer);
+					timer = setTimeout(use_bait_item, rng(ACTION_DELAY_FISH_START));
+				}
+				else // 10k filet // 3 error sysmsgs at once for that lol
+				{
+					command.message("You have reached the 10k dismantled fish parts limit, stopping");
+					console.log("You have reached the 10k dismantled fish parts limit, stopping");
+					Stop();
+				}
+			}
+			else if(msg.id === 'SMT_CANNOT_FISHING_FULL_INVEN') // full inven
+			{
+				command.message("Inventory full, lets dismantle fish!");
+				clearTimeout(timer);
+				timer = setTimeout(cleanup_by_dismantle, rng(ACTION_DELAY_FISH_START));
+			}
+			else if(msg.id === 'SMT_CANNOT_FISHING_NON_AREA' && !negoWaiting) // server trolling us?
+			{
+				command.message("Fishing area changed (you left it?), well that happens... lets try again?");
+				console.log("Fishing area changed (you left it?), well that happens... Retrying...");
+				clearTimeout(timer);
+				leftArea++;
+				if(leftArea < 6)
+				{
+					timer = setTimeout(throw_the_rod, rng(ACTION_DELAY_THROW_ROD));
+				}
+				else
+				{
+					Stop();
+					command.message("Fishing area changed for good it seems, can't fish anymore, stopping");
+					console.log("Fishing area changed for good it seems, can't fish anymore, stopping");
+				}
+			}
+			else if(msg.id === 'SMT_FISHING_RESULT_CANCLE') // hmmm?
+			{
+				command.message("Fishing cancelled... lets try again?");
+				console.log("Fishing cancelled... due to lag? Retrying...");
+				clearTimeout(timer);
+				timer = setTimeout(throw_the_rod, rng(ACTION_DELAY_THROW_ROD));
+			}
+			else if(negoWaiting && !pendingDeals.length && msg.id === 'SMT_MEDIATE_SUCCESS_SELL') // all out of deals and still waiting?
+			{
+				command.message('All negotiations finished... resuming fishing shortly')
+				//console.log("nego end wait OK");
+				clearTimeout(timer);
+				timer = setTimeout(throw_the_rod, (rng(ACTION_DELAY_THROW_ROD)+1000));
+			}
+			else if(msg.id === 'SMT_CANNOT_USE_ITEM_WHILE_CONTRACT') // we want to throw the rod but still trading?
+			{
+				negoWaiting = true;
+				command.message('Negotiations are taking long time to finish... lets wait a bit more')
+				//console.log("nego long wait");
+				clearTimeout(timer);
+				timer = setTimeout(throw_the_rod, (rng(ACTION_DELAY_THROW_ROD)+3000));
+			}
+        });
+	}
+	
+	function unload()
+	{
+		if(hooks.length)
+		{
+			for(let h of hooks) mod.unhook(h);
+			hooks = [];
+		}
+	}
+
+	function Hook()
+	{
+		hooks.push(mod.hook(...arguments));
+	}
+}
